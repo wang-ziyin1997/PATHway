4258fa651901af034b6a6ce0d80ddd714f0450d9
wang-ziyin1997 <794629151@qq.com>
2023-04-03 15:10:59 -0400

    Add Visualization Functions
    Function GSEA_histogram: top20 pathways ranked by enrichment score.
    Function GSEA_bubble: top10 pathways ranked by p value.

--- R/Pathway.R
+++ R/Pathway.R
@@ -1,14 +1,19 @@
 #' GSEA Analysis on log2FC ranked DEGs
 #'
 #' @title GSEAnalysis
-#' @description
-#' @param data
-#' @param outpath
-#' @importFrom clusterProfiler GSEA
-#' @import enrichplot
-#' @import magrittr
+#' @description Generates GSEA results and GSEA plots for the top ten pathways
+#'   sorted by p-value and Enrichment Score, respectively.
+#' @param data a named gene list with EntrezID format sorted by log2FC.
+#' @param outpath output pathway
+#' @importFrom clusterProfiler GSEA setReadable
+#' @importFrom dplyr group_by summarise filter mutate arrange slice left_join bind_rows
+#' @importFrom enrichplot gseaplot2
+#' @importFrom org.Hs.eg.db org.Hs.eg.db
+#' @importFrom stringr str_count
+#' @importFrom grDevices png dev.off
+#' @importFrom readr write_csv
 #'
-#' @return
+#' @return a set of GSEA plots and a data table
 #' @export
 GSEAnalysis <- function(
     data,
@@ -28,10 +33,6 @@ GSEAnalysis <- function(
   if (file.exists(gseapath)==FALSE){
     dir.create(gseapath)}
 
-  data(c2)
-  data(go.bp)
-  data(go.cc)
-  data(go.mf)
   data_lists <- list(c2, go.bp, go.cc, go.mf)
   result_list <- list()
   Class <- c('C2','GO.BP','GO.CC','GO.MF')
@@ -49,7 +50,7 @@ GSEAnalysis <- function(
 
     up_regulated <- df.gsea %>%
       dplyr::filter(up_down == "up-regulated") %>%
-      dplyr::arrange(desc(enrichmentScore)) %>%
+      dplyr::arrange(dplyr::desc(enrichmentScore)) %>%
       dplyr::slice(1:min(10, nrow(.)))
     down_regulated <- df.gsea %>%
       dplyr::filter(up_down == "down-regulated") %>%
@@ -87,6 +88,7 @@ GSEAnalysis <- function(
       print(paste(as.character(count),'/',len))}
   }
   res <- dplyr::bind_rows(result_list)
+  readr::write_csv(res, paste(outpath,"GSEA_results.csv",sep='/'))
   return(res)
 }
 
@@ -119,3 +121,136 @@ DEGsData <- function(file,
   rnk.list <- sort(rnk.list,decreasing = T)
   return(rnk.list)
 }
+
+#' GSEA Histogram Plotting
+#'
+#' This function generates a GSEA histogram plot using the input data
+#' and saves it to the specified output path.
+#'
+#' @param data A data frame containing the following columns: 'ID',
+#' 'enrichmentScore', 'up_down', and 'classification'.
+#' @param outpath A string representing the output path where
+#' the histogram plot will be saved.
+#'
+#' @importFrom dplyr mutate if_else group_by arrange case_when desc slice_head ungroup
+#' @importFrom ggplot2 ggplot aes geom_bar facet_grid scale_fill_manual theme
+#' element_blank ylab xlab scale_y_discrete ggsave
+#' @importFrom stringr str_sub str_wrap
+#' @importFrom magrittr %>%
+#' @importFrom stats reorder
+#'
+#' @export
+GSEA_histogram <- function(data, outpath){
+  if (missing(data)) {
+    stop("Data not provided. Please provide a valid input.")
+  }
+  required_columns <- c('ID', 'enrichmentScore', 'up_down', 'classification')
+  if (!all(required_columns %in% colnames(data))) {
+    stop("Input data frame must contain the following columns: ",
+         paste(required_columns, collapse = ", "), ".")
+  }
+  if (missing(outpath)) {
+    stop("Output path not provided. Please provide a valid input.")
+  }
+
+  if (file.exists(outpath)==FALSE){
+    dir.create(outpath)
+  }
+  bar.data <- data %>%
+    dplyr::mutate(
+      Path = gsub("_", " ", ID),
+      label = stringr::str_sub(Path, 1, 45)
+    ) %>%
+    dplyr::mutate(
+      label = dplyr::if_else(nchar(label) >= 40, paste0(label, "..."), label)
+    )
+
+  result <- bar.data %>%
+    dplyr::group_by(classification, up_down) %>%
+    dplyr::arrange(dplyr::case_when(up_down == "up" ~ dplyr::desc(enrichmentScore),
+                                    up_down == "down" ~ enrichmentScore)) %>%
+    dplyr::slice_head(n = min(20, nrow(.))) %>%
+    dplyr::ungroup()
+
+  barplot <- ggplot2::ggplot(result, ggplot2::aes(x = enrichmentScore, y = stats::reorder(label, enrichmentScore), fill = up_down)) +
+    ggplot2::geom_bar(stat = "identity", position = "identity", colour = "black", linewidth = 0.1) +
+    ggplot2::facet_grid(classification~., scales = "free")+
+    ggplot2::scale_fill_manual(values = c("#CCEEFF", "#FFDDDD"), guide = "none") +
+    ggplot2::theme(panel.grid.major = ggplot2::element_blank(),panel.grid.minor = ggplot2::element_blank()) +
+    ggplot2::ylab('Pathway')+ ggplot2::xlab('Enrichment Score') +
+    ggplot2::scale_y_discrete(labels=function(x) stringr::str_wrap(x, width=50))
+  ggplot2::ggsave(barplot,filename = paste(outpath,"bar_plot.png",sep='/'), width = 10, height = 18)
+}
+
+#' GSEA Bubble Plot
+#'
+#' This function creates a GSEA bubble plot for each specified classification
+#' ('C2', 'GO.BP', 'GO.CC', 'GO.MF') and saves it to the provided output path.
+#'
+#' @param data A data frame containing the required columns: 'ID',
+#' 'enrichmentScore', 'up_down', 'classification', 'GeneRatio', 'count',
+#' and 'pvalue'.
+#' @param outpath The output path where the bubble plot images will be saved.
+#'
+#' @return Saves the bubble plot images to the specified output path.
+#' @export
+#'
+#' @importFrom dplyr filter mutate group_by arrange case_when slice_head ungroup
+#' @importFrom ggplot2 ggplot aes geom_point scale_colour_gradient xlab ylab scale_y_discrete theme_void ggsave
+#' @importFrom stringr str_wrap
+#' @importFrom gridExtra grid.arrange
+GSEA_bubble <- function(data, outpath){
+  if (missing(data)) {
+    stop("Data not provided. Please provide a valid input.")
+  }
+  required_columns <- c('ID', 'enrichmentScore', 'up_down', 'classification','GeneRatio','count','pvalue')
+  if (!all(required_columns %in% colnames(data))) {
+    stop("Input data frame must contain the following columns: ",
+         paste(required_columns, collapse = ", "), ".")
+  }
+  if (missing(outpath)) {
+    stop("Output path not provided. Please provide a valid input.")
+  }
+
+  if (file.exists(outpath)==FALSE){
+    dir.create(outpath)
+  }
+
+  for(i in c('C2','GO.BP','GO.CC','GO.MF')){
+    subdata <- data %>%
+      dplyr::filter(classification == i) %>%
+      dplyr::mutate(Path = gsub("_", " ", ID)) %>%
+      dplyr::group_by(up_down) %>%
+      dplyr::arrange(dplyr::case_when(up_down == "up" ~ pvalue,
+                                      up_down == "down" ~ pvalue)) %>%
+      dplyr::slice_head(n = min(10, nrow(.))) %>%
+      dplyr::ungroup()
+
+    empty_plot <- ggplot2::ggplot() + ggplot2::theme_void()
+    up_plot <- empty_plot
+    down_plot <- empty_plot
+    if (any(subdata$up_down == "up-regulated")) {
+      up_plot <- ggplot2::ggplot(subdata[subdata$up_down == "up-regulated", ], ggplot2::aes(x = GeneRatio, y = reorder(Path, GeneRatio))) +
+        ggplot2::geom_point(aes(size = count, color = `pvalue`)) +
+        ggplot2::scale_colour_gradient(low = "red", high = "blue") +
+        ggplot2::xlab("Gene Ratio: numbers of overlap gene/numbers of genes in the pathway") +
+        ggplot2::ylab("Pathways (Up)") +
+        ggplot2::scale_y_discrete(labels = function(x) stringr::str_wrap(x, width = 40))
+    }
+
+    if (any(subdata$up_down == "down-regulated")) {
+      down_plot <- ggplot2::ggplot(subdata[subdata$up_down == "down-regulated", ], ggplot2::aes(x = GeneRatio, y = reorder(Path, GeneRatio))) +
+        ggplot2::geom_point(aes(size = count, color = `pvalue`)) +
+        ggplot2::scale_colour_gradient(low = "red", high = "blue") +
+        ggplot2::xlab("Gene Ratio: numbers of overlap gene/numbers of genes in the pathway") +
+        ggplot2::ylab("Pathways (Down)") +
+        ggplot2::scale_y_discrete(labels = function(x) stringr::str_wrap(x, width = 40))
+    }
+
+    bbp <- gridExtra::grid.arrange(up_plot, down_plot, ncol = 1)
+
+    ggplot2::ggsave(bbp,filename = paste(outpath,'/bubble.plot.',i,'.png',sep=''), width = 7.5, height = 10.5)
+    print(paste(i,'bubble plot saved.'))
+  }
+}
+
---
+++ R/PathwayGenes.R
@@ -0,0 +1,48 @@
+#' C2
+#'
+#' This Gene sets are derived from diverse sources,
+#' such as online pathway databases and biomedical literature,
+#' and are curated by individual domain experts.
+#' Each gene set page specifies the source of the corresponding gene set.
+#' The C2 collection is categorized into two subcollections:
+#' Chemical and Genetic Perturbations (CGP) and Canonical Pathways (CP).
+#'
+#' @name c2
+#' @docType data
+#' @usage data(c2)
+#' @format A data frame with 148489 rows and 2 columns.
+#' @source c2.cp.v7.5.1.symbols.gmt
+NULL
+
+#' GO.BP
+#'
+#' Biological Process from Gene Ontology (GO)
+#'
+#' @name go.bp
+#' @docType data
+#' @usage data(c2)
+#' @format A data frame with 631855 rows and 2 columns.
+#' @source c5.go.bp.v7.5.1.symbols.plusCAF_Matrisome.gmt
+NULL
+
+#' GO.CC
+#'
+#' Cellular Component (CC) from Gene Ontology (GO)
+#'
+#' @name go.cc
+#' @docType data
+#' @usage data(c2)
+#' @format A data frame with 99098 rows and 2 columns.
+#' @source c5.go.cc.v7.5.1.symbols.gmt
+NULL
+
+#' GO.MF
+#'
+#' Molecular Function (MF) from Gene Ontology (GO)
+#'
+#' @name go.mf
+#' @docType data
+#' @usage data(c2)
+#' @format A data frame with 108789 rows and 2 columns.
+#' @source c5.go.mf.v7.5.1.symbols.plusCAF_Matrisome.gmt
+NULL
